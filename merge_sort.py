08878c86aa1d4fc561db15da1c8f466581413606
Alec Yalcin <alecyalcin@gmail.com>
2024-04-24 14:54:40 -0300

    Criado merge_sort, alterado quick_sort

---
+++ merge_sort.py
@@ -0,0 +1,30 @@
+import random as rm
+
+def merge_sort(A, start, end):
+    if start < end:
+        middle = (start + end)//2
+
+        merge_sort(A, start, middle)
+        merge_sort(A, middle+1, end)
+        merge(A, start, middle, end)
+
+def merge(A, start, middle, end):
+    i = start
+    j = middle + 1
+
+    v = [0]*len(A)
+    for k in range(0, end-start):
+        if (i <= middle) and ((j>end) or (A[i] < A[j])):
+            v[k] = A[i]
+            i += 1
+        else:
+            v[k] = A[j]
+            j += 1
+    for k in range(0, end-start):
+        A[start+k] = v[k]
+
+A = [rm.randint(0, 10) for x in range(0,10)]
+merge_sort(A, 0, len(A)-1)
+print(f"A_merged = {A}")
+
+
--- quick_sort.py
+++ quick_sort.py
@@ -1,22 +1,22 @@
 import random as rm
 
-def partition(arr, start, end):
-    pivot = arr[end]
+def partition(A, start, end):
+    pivot = A[end]
     i = start - 1
     for j in range(start, end):
-        if arr[j] <= pivot:
+        if A[j] <= pivot:
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[end] = arr[end], arr[i + 1]
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[end] = A[end], A[i + 1]
     return i + 1
 
-def quick_sort_recursive(arr, start, end):
+def quick_sort(A, start, end):
     if start < end:
-        pivot_index = partition(arr, start, end)
-        quick_sort_recursive(arr, start, pivot_index - 1)
-        quick_sort_recursive(arr, pivot_index + 1, end)
+        pivot = partition(A, start, end)
+        quick_sort(A, start, pivot - 1)
+        quick_sort(A, pivot + 1, end)
 
 # Exemplo de uso:
-arr = [rm.randint(1, 100) for i in range(rm.randint(10,15))]
-quick_sort_recursive(arr, 0, len(arr) - 1)
-print("Sorted array (recursive):", arr)
+A = [rm.randint(1, 100) for i in range(rm.randint(10,15))]
+quick_sort(A, 0, len(A) - 1)
+print("Sorted Aay (recursive):", A)

b8ba43af8a358207a2386cf6c9dca935dfec8ec6
alecyalcin <alecyalcin@gmail.com>
2024-08-11 13:43:28 -0300

    Hash Table e Arvore Binaria

---
+++ fontes/binary_tree_mean.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct node {
+    int value;
+    struct node* left;
+    struct node* right;
+} Node;
+
+Node* create_node(int value) {
+    // Inicializando novo nó
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    // Adicionando valores
+    new_node->value = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    // Retornando o nó
+    return new_node;
+}
+
+void insert_node(Node** root, int value) {
+    // Se o nó é válido
+    if((*root) != NULL) {
+        // Se o valor adicionado é menor que o valor do nó
+        if ((*root)->value > value) {
+            insert_node(&(*root)->left, value);
+        } else {
+            insert_node(&(*root)->right, value);
+        }
+    } else {
+        (*root) = create_node(value);
+    }
+}
+
+Node* binary_search(Node* root, int number) {
+    if (root != NULL) {
+        // Se o valor é igual ao número
+        if(root->value == number) {
+            return root;
+        }
+        // Se o valor é maior que o número  
+        if(root->value > number) {
+            return binary_search(root->left, number);
+        }
+        // Se o valor é menor que o número
+        return binary_search(root->right, number);
+    }
+    // Se não existe o valor procurado
+    return NULL;
+}
+
+void print_tree(Node* root) {
+    if (root != NULL) {
+        print_tree(root->left);
+        printf("%d\n", root->value);
+        print_tree(root->right);
+    }
+}
+
+int main(int argc, char **argv) {
+    // Criando a raiz da árvore binária
+    Node* root = NULL;
+
+    // Tamanho do vetor/árvore binária
+    unsigned int n = atoi(argv[1]);
+
+    // Inicializador de randomização
+    srand(time(NULL));
+    
+    // inteiro auxiliar para pegar último valor gerado na lista
+    int last;     
+    // Inserindo elemento aleatórios na árvore binária
+    for(int i = 0; i < n; i++) {
+        last = (rand() % n) + 1;
+        insert_node(&root, last);
+    }
+
+    // Inicializando variáveis de tempo
+    struct timespec start, end;
+
+    // Guardando valores de tempo
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Node* aux = binary_search(root, last);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // liberando aux
+    free(aux);
+
+    // Calculando o tempo
+    unsigned  time = (end.tv_sec * 1e9 + end.tv_nsec) - (start.tv_sec * 1e9 + start.tv_nsec);
+
+    // Mostrando o tempo para iterate
+    printf("%u\n", time);
+
+    return 0;
+}
---
+++ fontes/hash_table_worst.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Estrutura de Nó
+typedef struct node {
+    int value;
+    struct node* next;
+} Node;
+
+// Estrutura de Tabela Hash
+typedef struct hashTable {
+    unsigned int m; // Tamanho da tabela
+    unsigned int n;     // Quantidade de Elementos
+    Node** nodes;
+} HashTable;
+
+// Protótipos das funções
+void insert(HashTable* hashTable, int value);
+void reHash(HashTable *hashTable);
+int search(HashTable *hashTable, int value);
+void delete(HashTable *hashTable, int value);
+
+// Função Hash - Para transformar o valor da chave dada em uma chave da lista
+unsigned int hash(int value, int m) {
+    // Se o valor da operação for negativo, adicione +m, se não, devolva a operação
+    return value % m < 0 ? (value % m) + m : value % m;
+}
+
+// Função Re-hash - Para aumentar a quantidade de espaços na tabela e evitar que o tempo não seja constante
+void reHash(HashTable* hashTable) {
+    // Pegando a lista anteriormente salva
+    unsigned int old_m = hashTable->m;
+    Node** old_nodes = hashTable->nodes;
+
+    // Reconfigurar Tabela e Dobrar o Tamanho
+    hashTable->m *= 2;  
+    hashTable->n = 0;
+    hashTable->nodes = (Node**)malloc(sizeof(Node*) * hashTable->m);
+
+    // Nulificando endereços da lista dentro da tabela
+    for(int i = 0; i < hashTable->m; i++) {
+        hashTable->nodes[i] = NULL;
+    }
+
+    // Inserindo antigos valores na lista
+    for(int i = 0;i < old_m; i++) {
+        Node* aux = old_nodes[i];
+        while(aux != NULL) {
+            // Inserção dos elementos
+            insert(hashTable, aux->value);
+
+            // Variável para liberar memória alocada
+            Node* toFree = aux;
+
+            // Atualizando valor de aux;
+            aux = aux->next;
+
+            // Liberando rmemória antiga
+            free(toFree);
+        }
+    }
+    // Liberando memória da antiga lista
+    free(old_nodes);
+}
+
+// Função para calcular o fator de carga - Usado para prever se a lista está constante ou não
+float loadFactor(HashTable* hashTable) {
+    return (float)hashTable->n / hashTable->m;
+}
+
+// Criando um novo nó
+Node* create_node(int value) {
+    // Alocando memória no novo nó
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    // Alterando valores internos do nó
+    node->value = value;
+    node->next = NULL;
+
+    // Retronando o nó
+    return node;
+}
+
+// Inicialização de Tabela Hash
+HashTable* create_table(unsigned int m) {
+    // Alocando memória na nova tabela
+    HashTable* hashTable = (HashTable*)malloc(sizeof(HashTable));
+
+    // Alterando valores internos da tabela
+    hashTable->m = m;
+    hashTable->n = 0;
+    hashTable->nodes = (Node**)malloc(sizeof(Node*) * m);
+
+    // Nulificando endereços da lista dentro da tabela
+    for(int i = 0; i < m; i++) {
+        hashTable->nodes[i] = NULL;
+    }
+
+    // Retornando a tabela
+    return hashTable;
+}
+
+// Inserindo elementos na Tabela Hash
+void insert(HashTable* hashTable, int value) {
+    // Transformando o elemento em uma chave - Esse processo não é necessário caso o Inserta já indique um chave
+    unsigned int key = hash(value, hashTable->m);
+
+    // Como ser é o pior caso, então não há o rehash.
+    if(1) {
+        // Índice de acordo com a chave escolhida (ou transformada)
+        unsigned int index = hash(key, hashTable->m);
+
+        // Novo nó para ser inserido
+        Node* new_node = create_node(value);
+
+        // Verificando se, na tabela, o índice já possui algum valor
+        if(hashTable->nodes[index] == NULL) {
+            hashTable->nodes[index] = new_node;
+        } else {
+            // Pegando o node do índice atual
+            Node* index_node = hashTable->nodes[index];
+            // Procurnado o próximo índice da lista até não existir
+            while(index_node->next != NULL) {
+                index_node = index_node->next;
+            }
+            // Adicionando novo nó na lista
+            index_node->next = new_node;
+        }
+
+        // Aumenta o valor da quantidade de itens na tabela
+        hashTable->n++;
+    // Se o fator de carga for maior ou igual a 1, refaça a lista maior
+    } else {
+        reHash(hashTable);
+        insert(hashTable, value);
+    }
+}
+
+// Procurando elementos na tabela Hash
+int search(HashTable* hashTable, int value) {
+    // Pegando elemento e transformando em chave
+    unsigned int index = hash(value, hashTable->m); 
+    Node* aux = hashTable->nodes[index];   
+
+    while(aux != NULL) {
+        if(aux->value == value) {
+            return index;
+        }
+        aux = aux->next;
+    }
+
+    return -1;
+}
+
+// Excluindo elementos na tablea Hash
+
+// Printar a tabela completa
+void printHashTable(HashTable* hashTable) {
+    for(int i = 0; i < hashTable->m; i++) {
+        printf("[%d] -> ", i);
+        Node* temp = hashTable->nodes[i];
+        while(temp != NULL) {
+            printf("(%d) -> ", temp->value);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main(int argc, char **argv) {
+    // Quantidade de elementos na tabela
+    unsigned int n = atoi(argv[1]);
+
+    // Criando tabela hash com o tamanho fornecido pelo usuário
+    HashTable* hashTable = create_table(1);
+
+    // Inicializador de randomização
+    srand(time(NULL));
+
+    // inteiro auxiliar para ter acesso ao último elemento da lista
+    unsigned int last = 1;
+    
+    // Inserindo elemento aleatório na árvore binária
+    for(int i = 0; i < n; i++) {
+        last = (rand() % (n*10)) + 1;
+        insert(hashTable, last);
+    }
+
+    // Inicializando variáveis de tempo
+    struct timespec start, end;
+
+    // Guardando valores de tempo
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    int aux = search(hashTable, last);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // Calculando o tempo
+    unsigned  time = (end.tv_sec * 1e9 + end.tv_nsec) - (start.tv_sec * 1e9 + start.tv_nsec);
+
+    // Mostrando o tempo para iterate
+    printf("%u\n", time);
+
+    return 0;
+}
